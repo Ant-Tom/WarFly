aec04e67bf1c9a1dd6d179fc66909c64246c1416
Anton <146194876+Ant-Tom@users.noreply.github.com>
2024-06-25 07:27:07 +0300

    coreMotion

--- Cloud.swift
+++ Cloud.swift
@@ -19,6 +19,7 @@ final class Cloud: SKSpriteNode, GameBackgroundSpriteable {
         cloud.setScale(randomScaleFactor())
         cloud.position = point
         cloud.zPosition = 10
+        cloud.run(move(from: point)) 
         
         return cloud
     }
@@ -36,6 +37,14 @@ final class Cloud: SKSpriteNode, GameBackgroundSpriteable {
         let randomNumber = CGFloat(distribution.nextInt()) / 10
         
         return randomNumber
+    }
+    
+    fileprivate static func move (from point: CGPoint) -> SKAction {
+        let movePoint = CGPoint(x: point .x, y: -200)
+        let moveDistance = point.y + 200
+        let movementSpeed: CGFloat = 15.0
+        let duration = moveDistance / movementSpeed
+        return SKAction.move(to: movePoint, duration: TimeInterval(duration))
     }
     
 }
