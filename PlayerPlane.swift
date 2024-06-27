60b5d6a65d4362827e2c627ebc0d77b067259914
Anton <146194876+Ant-Tom@users.noreply.github.com>
2024-06-27 07:30:29 +0300

    cleaning

--- PlayerPlane.swift
+++ PlayerPlane.swift
@@ -6,15 +6,41 @@
 //
 
 import SpriteKit
+import CoreMotion
 
 class PlayerPlane: SKSpriteNode {
+    
+    let motionManager = CMMotionManager()
+    var xAcceleration: CGFloat = 0
+    let screenSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
 
-    static func populate(at point: CGPoint) -> SKSpriteNode {
+    static func populate(at point: CGPoint) -> PlayerPlane {
         let playerPlaneTexture = SKTexture(imageNamed: "airplane_3ver2_13")
-        let playerPlane = SKSpriteNode(texture: playerPlaneTexture)
+        let playerPlane = PlayerPlane(texture: playerPlaneTexture)
         playerPlane.setScale(0.5)
         playerPlane.position = point
         playerPlane.zPosition = 20
         return playerPlane
+    }
+    
+    func checkPosition() {
+        
+        self.position.x += xAcceleration * 50
+        
+        if self.position.x < -70 {
+            self.position.x = screenSize.width + 70
+        } else if self.position.x > screenSize.width + 70 {
+            self.position.x = -70
+        }
+    }
+    
+    func performFly() {
+        motionManager.accelerometerUpdateInterval = 0.2
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
+            if let data = data {
+                let acceleration = data.acceleration
+                self.xAcceleration = CGFloat(acceleration.x) * 0.7 + self.xAcceleration * 0.3
+            }
+        }
     }
 }
